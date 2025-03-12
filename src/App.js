01b9c2beb197ae4295ac303dd2d663003c542d40
Caio-GBrayner <caiogomesbrayner@gmail.com>
2025-03-12 15:05:44 -0300

    Intalled -> React-icons, ReactBootstrap and deleted useless files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BsCalendar2CheckFill} from "react-icons/bs";
+import { Button, Col, Container, Row } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row>
+          <Col>
+          <h1 className='text-center fw-light mt-3'><BsCalendar2CheckFill/> Appointments</h1>
+          <div className='d-grid gap-2'>
+          <Button size='sm'>Hello world</Button>
+          </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={3}>
+          <h3>Hello, bootstrap</h3>
+          </Col>
+          <Col md={3}>
+          <h3>Hello, bootstrap</h3>
+          </Col>
+          <Col md={3}>
+          <h3>Hello, bootstrap</h3>
+          </Col>
+          <Col md={3}>
+          <h3>Hello, bootstrap</h3>
+          </Col>
+        </Row>
+      </Container>
+      <h1><BsCalendar2CheckFill/>Appointments</h1>
+      <Button>Hello world</Button>
     </div>
   );
 }
