023e21ac81c2091b3b7f451754e70cc8b925c021
Caio-GBrayner <caiogomesbrayner@gmail.com>
2025-03-14 11:45:20 -0300

    AddAppointment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BsCalendar2CheckFill} from "react-icons/bs";
-import { Button, Col, Container, Row } from 'react-bootstrap';
+import {Col, Container, Row } from 'react-bootstrap';
 import Search from "./components/Search";
+import AddAppiontment from './components/AddAppointment';
 
 function App() {
   return (
@@ -12,28 +13,15 @@ function App() {
           <h1 className='text-center fw-light mt-3'><BsCalendar2CheckFill/> Appointments</h1>
           </Col>
         </Row>
+        <Row className='justify-content-center'>
+          <AddAppiontment/>
+        </Row>
         <Row className='justify-content-center'> 
           <Col md={4}>
             <Search/>
           </Col>
         </Row>
-        <Row>
-          <Col md={3}>
-          <h3>Hello, bootstrap</h3>
-          </Col>
-          <Col md={3}>
-          <h3>Hello, bootstrap</h3>
-          </Col>
-          <Col md={3}>
-          <h3>Hello, bootstrap</h3>
-          </Col>
-          <Col md={3}>
-          <h3>Hello, bootstrap</h3>
-          </Col>
-        </Row>
       </Container>
-      <h1><BsCalendar2CheckFill/>Appointments</h1>
-      <Button>Hello world</Button>
     </div>
   );
 }
